a1a6ea2334d4b45289b7621c4f9be202883ebb73
Twilight9000 <70115364+Twilight9000@users.noreply.github.com>
2023-01-23 21:59:15 -0600

    Update PlayerBehavior.cs

--- Cat_Burglar/Assets/Scripts/PlayerBehavior.cs
+++ Cat_Burglar/Assets/Scripts/PlayerBehavior.cs
@@ -1,18 +1,92 @@
+/* Author: Jess Peters
+ * Date: 1/23/23
+ * Description: Contains the controls for the different control schemes. 
+ * 
+ */
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    private int currentControls = 1;
+    public Camera mainCam;
+    private Vector3 middlePos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        middlePos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentControls = 1;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentControls = 2;
+            transform.position = middlePos;
+
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentControls = 3;
+            transform.position = middlePos;
+
+        }
+
+
+        switch (currentControls)
+        {
+            case 1:
+                Scheme1();
+                break;
+
+            case 2:
+                Scheme2();
+                break;
+
+            case 3:
+                Scheme3();
+                break;
+                
+        }         
     }
+
+    /// <summary>
+    /// The Vents control scheme.
+    /// </summary>
+    void Scheme1()
+    {
+
+    }
+
+    /// <summary>
+    /// The FPS control scheme.
+    /// </summary>
+    void Scheme2()
+    {
+        
+
+    }
+
+    /// <summary>
+    /// The Point-And-Click control scheme.
+    /// </summary>
+    void Scheme3()
+    {
+
+    }
+
+
+
+
 }
