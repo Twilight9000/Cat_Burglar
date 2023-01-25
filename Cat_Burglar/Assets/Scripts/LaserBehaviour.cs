39a93cc11b9f5d3420718d5c0b0a235edb086a60
Draxnos <Hajny.shane@gmail.com>
2023-01-25 00:01:26 -0600

    Code Comments

--- Cat_Burglar/Assets/Scripts/LaserBehaviour.cs
+++ Cat_Burglar/Assets/Scripts/LaserBehaviour.cs
@@ -1,12 +1,23 @@
+/*********************************
+* File Name: LaserBehaviour
+* 
+* Author: Shane Hajny
+* 
+* Summary: When holding left mouse
+* button down, moves the dot to
+* where the player directs it.
+*********************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LaserBehaviour : MonoBehaviour
 {
+    //Necessary initializers
     public Camera cam;
     public CameraBehavior cb;
 
+    //Currently needs to be setup in scene view
     public GameObject dot;
 
     public string reflect = "Reflective";
@@ -21,15 +32,18 @@ public class LaserBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //If holding left mouse button
         if (Input.GetKey(KeyCode.Mouse0))
         {
             Ray ray;
             RaycastHit hit;
 
+            //If in the locked camera positions...
             if (cb.currentControls == 1 || cb.currentControls == 3)
             {
                 ray = cam.ScreenPointToRay(Input.mousePosition);
             }
+            //If in first person...
             else
             {
                 ray = new Ray(cam.transform.position, cam.transform.forward);
@@ -37,10 +51,12 @@ public class LaserBehaviour : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
+                //If reflection is to occur
                 if (hit.collider.gameObject.CompareTag(reflect))
                 {
                     ReflectDot(ray.direction, hit.normal, hit.point);
                 }
+                //Else, move dot to initial position
                 else
                 {
                     dot.transform.position = hit.point;
@@ -58,8 +74,10 @@ public class LaserBehaviour : MonoBehaviour
         Ray ray = new Ray(reflectPoint, Vector3.Reflect(inDir, normal));
         RaycastHit hit;
 
+        //Just the raycast setup from before
         if (Physics.Raycast(ray, out hit))
         {
+            //Except it loops here if it should reflect again
             if (hit.collider.gameObject.CompareTag(reflect))
             {
                 ReflectDot(ray.direction, hit.normal, hit.point);
