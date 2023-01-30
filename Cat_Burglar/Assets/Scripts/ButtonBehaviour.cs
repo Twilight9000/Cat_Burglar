fb0a1e97a3b43ff4ef14d7a663cb891ddab6c733
Casey Vande Hey <cvandehey@mail.bradley.edu>
2023-01-30 10:26:40 -0600

    Cat push button

--- Cat_Burglar/Assets/Scripts/ButtonBehaviour.cs
+++ Cat_Burglar/Assets/Scripts/ButtonBehaviour.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 public class ButtonBehaviour : MonoBehaviour
 {
     public GameObject door;
+    public float speed = 2;
+
+    Vector3 currentPosition;
+    Vector3 newPosition;
+
+    void Start()
+    {
+        float x = door.transform.position.x;
+        float y = door.transform.position.y + 10;
+        float z = door.transform.position.z;
+
+        currentPosition = door.transform.position;
+        newPosition = (currentPosition);
+        newPosition.y = newPosition.y + 10;
+    }
 
     void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.tag == "Cat")
         {
-            Destroy(door);
+            door.transform.position = Vector3.MoveTowards(currentPosition, newPosition, speed);
         }
     }
 }
