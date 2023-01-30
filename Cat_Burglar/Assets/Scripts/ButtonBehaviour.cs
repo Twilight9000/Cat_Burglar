95fee9e840d3b8f611eff558cc1956b931aa7642
Casey Vande Hey <cvandehey@mail.bradley.edu>
2023-01-30 10:01:44 -0600

    ye

--- Cat_Burglar/Assets/Scripts/ButtonBehaviour.cs
+++ Cat_Burglar/Assets/Scripts/ButtonBehaviour.cs
@@ -6,18 +6,6 @@ public class ButtonBehaviour : MonoBehaviour
 {
     public GameObject door;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.tag == "Cat")
