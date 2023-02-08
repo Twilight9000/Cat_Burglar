cf4b4a19ed37d3841c1a5ebb522dffbcc7c73ca9
Draxnos <Hajny.shane@gmail.com>
2023-02-08 10:36:58 -0600

    e

--- Cat_Burglar/Assets/Scripts/MapItems/ButtonBehaviour.cs
+++ Cat_Burglar/Assets/Scripts/MapItems/ButtonBehaviour.cs
@@ -4,30 +4,19 @@ using UnityEngine;
 
 public class ButtonBehaviour : MonoBehaviour
 {
-    public GameObject door;
-    public float speed = 2;
-    public bool move;
+    public GameObject obj;
+    public IInteractable activate;
 
-    public Transform newPosition;
-
-    void Start()
+    private void Start()
     {
-        move = false;
-    }
-
-    void Update()
-    {
-        if(move)
-        {
-            door.transform.position = Vector3.MoveTowards(door.transform.position, newPosition.position, speed * Time.deltaTime);
-        }
+        activate = obj.GetComponent<IInteractable>();
     }
 
     void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            move = true;
+            activate.Interact();
         }
     }
 }
