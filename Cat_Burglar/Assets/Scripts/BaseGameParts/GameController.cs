d02404ac6d2eaf82703fd0b0a842e8368ac62ce2
ogNINJAGOLDBOX <gslye@mail.bradley.edu>
2023-02-13 10:52:22 -0600

    GameController TooHeavyFix

--- Cat_Burglar/Assets/Scripts/BaseGameParts/GameController.cs
+++ Cat_Burglar/Assets/Scripts/BaseGameParts/GameController.cs
@@ -13,7 +13,7 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     public string SceneName;
-    public GameObject PauseMenu, DiamondObj, WinText;
+    public GameObject PauseMenu, DiamondObj, WinText, WeightLimitMenu;
     public Text totalMoneyText, moneyCarriedText;
 
     /// <summary>
@@ -219,5 +219,12 @@ public class GameController : MonoBehaviour
 
     }
 
+    public IEnumerator WeightLimitReached()
+    {
+        WeightLimitMenu.SetActive(true);
 
+        yield return new WaitForSeconds(3f);
+
+        WeightLimitMenu.SetActive(false);
+    }
 }
